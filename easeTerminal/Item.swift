5f14cfb0cea544428fbb3da66af745dff73c673a
Louie <louisk02@icloud.com>
2026-03-12 18:50:54 +1300

    Initial Commit

---
+++ easeTerminal/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  easeTerminal
+//
+//  Created by Louis Kolodzinski on 12/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ easeTerminalTests/easeTerminalTests.swift
@@ -0,0 +1,17 @@
+//
+//  easeTerminalTests.swift
+//  easeTerminalTests
+//
+//  Created by Louis Kolodzinski on 12/03/2026.
+//
+
+import Testing
+@testable import easeTerminal
+
+struct easeTerminalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
